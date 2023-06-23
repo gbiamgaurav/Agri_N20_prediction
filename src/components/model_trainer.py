d03fd4ab80b7b4a7069aa24a2a0b5b89c43eb86c
gbiamgaurav <gaurav.bhattacharya10@gmail.com>
2023-06-23 13:57:19 +0530

    model_trainer completed

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,18 +1,19 @@
 import os
 import sys
 from dataclasses import dataclass
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score
-from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
+from sklearn.ensemble import RandomForestRegressor
 from src.exception import CustomException
 from src.logger import logging
 from src.utils.utils import save_object, evaluate_models
 from sklearn.model_selection import GridSearchCV
 
+
 @dataclass
 class ModelTrainerConfig:
     trained_model_file_path = os.path.join("artifacts", "model.pkl")
+
 
 class ModelTrainer:
     def __init__(self):
@@ -21,78 +22,69 @@ class ModelTrainer:
     def initiate_model_trainer(self, train_array, test_array):
         try:
             logging.info("Splitting training and test input data")
-
             X_train, y_train, X_test, y_test = (
                 train_array[:, :-1],
                 train_array[:, -1],
                 test_array[:, :-1],
                 test_array[:, -1]
             )
-
             models = {
-                "RandomForest": RandomForestRegressor(),
+                "Random Forest": RandomForestRegressor(),
                 "XGBRegressor": XGBRegressor()
+            }
+            params = {
+                "Random Forest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256, 300],
+                    "max_depth": [None, 5, 10],
+                    "min_samples_split": [2, 5, 10]
+                    
+                },
+
+                "XGBRegressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256, 300],
+                    "max_depth": [3, 5, 7],
+                }
+
             }
 
             logging.info("Evaluating models before hyperparameter tuning")
-            report_before_tuning = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
-            logging.info(f"Model evaluation before hyperparameter tuning: {report_before_tuning}")
+            model_report = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test,
+                                           models=models, param=params)
+            logging.info(f"Model evaluation before hyperparameter tuning: {model_report}")
 
-            param_grid = {
-                "RandomForest": {
-                    "n_estimators": [100, 200, 300],
-                    "max_depth": [None, 5, 10],
-                    "min_samples_split": [2, 5, 10],
-                },
-                "XGBRegressor": {
-                    "n_estimators": [100, 200, 300],
-                    "max_depth": [3, 5, 7],
-                    "learning_rate": [0.1, 0.01, 0.001],
-                },
-            }
-            
+            # To get the best model score from the dictionary
+            best_model_score = max(sorted(model_report.values()))
 
-            best_model_score = 0.0
-            best_model_name = ""
-            best_model = None
+            # To get the best model name from the dictionary
+            best_model_name = list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model = models[best_model_name]
 
-            for model_name, model in models.items():
-                logging.info(f"Performing hyperparameter tuning for {model_name}")
-
-                grid_search = GridSearchCV(
-                    estimator=model,
-                    param_grid=param_grid[model_name],
-                    scoring="r2",
-                    cv=5
-                )
-
-                grid_search.fit(X_train, y_train)
-
-                if grid_search.best_score_ > best_model_score:
-                    best_model_score = grid_search.best_score_
-                    best_model_name = model_name
-                    best_model = grid_search.best_estimator_
-
-            if best_model is None or best_model_score < 0.3:
+            if best_model_score < 0.5:
                 raise CustomException("No best model found")
 
             logging.info(f"Best model found on both training and testing dataset")
 
-            logging.info(f"Best parameters for {best_model_name}: {best_model.get_params()}")
+            logging.info(f"Hyperparameters for {best_model_name}: {best_model.get_params()}")
+
+            best_model.fit(X_train, y_train)  # Refit the model on the entire training data
 
             save_object(
-                file_path=self.model_trainer_config.trained_model_file_path, 
-                obj=best_model)
-
-            logging.info("Evaluating best model after hyperparameter tuning")
-            report_after_tuning = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models={best_model_name: best_model})
-            logging.info(f"Model evaluation after hyperparameter tuning: {report_after_tuning}")
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
 
             predicted = best_model.predict(X_test)
 
-            score = r2_score(y_test, predicted)
+            r2_square = r2_score(y_test, predicted)
 
-            return score
+            logging.info("Evaluating best model after hyperparameter tuning")
+            logging.info(f"Best model name: {best_model_name}")
+            logging.info(f"R2 score after hyperparameter tuning: {r2_square}")
+
+            return r2_square, best_model_name
 
         except Exception as e:
             raise CustomException(e, sys)
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,163 +1,59 @@
 import os
 import sys
-import numpy as np
+import numpy as np 
 import pandas as pd
+import dill
 from sklearn.metrics import r2_score
-from src.logger import logging
 from src.exception import CustomException
-from sklearn.ensemble import RandomForestRegressor
-from xgboost import XGBRegressor
-from sklearn.model_selection import train_test_split
-
-
-try:
-    import joblib
-    has_joblib = True
-except ImportError:
-    has_joblib = False
-    import pickle
-
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
+
         os.makedirs(dir_path, exist_ok=True)
-        
-        if has_joblib:
-            joblib.dump(obj, file_path)
-        else:
-            with open(file_path, "wb") as file_obj:
-                pickle.dump(obj, file_obj)
-            
+
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
-    
-    
-def load_object(file_path):
-    try:
-        if has_joblib:
-            obj = joblib.load(file_path)
-        else:
-            with open(file_path, "rb") as file_obj:
-                obj = pickle.load(file_obj)
-        return obj
-    
-    except Exception as e:
-        raise CustomException(e, sys)
-    
-    
-def evaluate_models(X_train, y_train, X_test, y_test, models, selected_model=None):
+
+
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
-        if selected_model:
-            model_names = [selected_model]
-            model_list = [(selected_model, models[selected_model])]
-        else:
-            model_names = models.keys()
-            model_list = models.items()
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-        for model_name, model in model_list:
-            if isinstance(model, (RandomForestRegressor, XGBRegressor)):
-                model.fit(X_train, y_train)  # Fit the model
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
-                y_train_pred = model.predict(X_train)
-                y_test_pred = model.predict(X_test)
+            #model.fit(X_train, y_train)  # Train model
 
-                train_model_score = r2_score(y_train, y_train_pred)
-                test_model_score = r2_score(y_test, y_test_pred)
+            y_train_pred = model.predict(X_train)
 
-                report[model_name] = test_model_score
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
 
         return report
 
     except Exception as e:
         raise CustomException(e, sys)
-
-
-# Main training logic
-def train_model(train_data, test_data):
+    
+def load_object(file_path):
     try:
-        logging.info("Splitting training and test input data")
-        X_train, X_test, y_train, y_test = train_test_split(
-            train_data[:, :-1],
-            train_data[:, -1],
-            test_size=0.2,
-            random_state=42
-        )
-
-        models = {
-            "RandomForest": RandomForestRegressor(),
-            "XGBRegressor": XGBRegressor()
-        }
-
-        logging.info("Evaluating models before hyperparameter tuning")
-        report_before_tuning = evaluate_models(
-            X_train=X_train,
-            y_train=y_train,
-            X_test=X_test,
-            y_test=y_test,
-            models=models
-        )
-        logging.info(f"Model evaluation before hyperparameter tuning: {report_before_tuning}")
-
-        param_grid = {
-            "RandomForest": {
-                "n_estimators": [100, 200, 300],
-                "max_depth": [None, 5, 10],
-                "min_samples_split": [2, 5, 10],
-            },
-            "XGBRegressor": {
-                "n_estimators": [100, 200, 300],
-                "max_depth": [3, 5, 7],
-                "learning_rate": [0.1, 0.01, 0.001],
-            },
-        }
-
-        best_model_score = 0.0
-        best_model_name = ""
-        best_model = None
-
-        for model_name, model in models.items():
-            logging.info(f"Performing hyperparameter tuning for {model_name}")
-
-            grid_search = GridSearchCV(
-                estimator=model,
-                param_grid=param_grid[model_name],
-                scoring="r2",
-                cv=5
-            )
-
-            grid_search.fit(X_train, y_train)
-
-            if grid_search.best_score_ > best_model_score:
-                best_model_score = grid_search.best_score_
-                best_model_name = model_name
-                best_model = grid_search.best_estimator_
-
-        if best_model is None or best_model_score < 0.5:
-            raise CustomException("No best model found")
-
-        logging.info(f"Best model found on both training and testing dataset")
-        logging.info(f"Best parameters for {best_model_name}: {best_model.get_params()}")
-
-        # Save the best_model using pickle
-        save_object("artifacts/model.pkl", best_model)
-
-        logging.info("Evaluating best model after hyperparameter tuning")
-        report_after_tuning = evaluate_models(
-            X_train=X_train,
-            y_train=y_train,
-            X_test=X_test,
-            y_test=y_test,
-            models={best_model_name: best_model}
-        )
-        logging.info(f"Model evaluation after hyperparameter tuning: {report_after_tuning}")
-
-        y_pred = best_model.predict(X_test)
-        score = r2_score(y_test, y_pred)
-
-        return score
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
